9cdc8280e4f4be1a57ccaf0f5b99e17e04a5e54b
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2023-03-28 16:28:50 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    console.log(req)
+    res.end('hello from the server')
+
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log("listening to request on port 8000");
+})
